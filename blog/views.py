24a614bdd995a6b5080a53062e82ef4524e89388
Annie Kwan <akwan.mtl@gmail.com>
2021-04-08 22:06:42 -0400

    make basic layout with materialize

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import PostForm
 
 # Create your views here.
+
+def index(request):
+  return render(request, 'blog/index.html', {
+    'header' : 'Hello this is a blog'
+  })
+
+def add(request):
+  return render(request, 'blog/add.html', {'form': PostForm()})
