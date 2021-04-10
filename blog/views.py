3ed5126b480aac85eaeafec725d3e2121c3a2202
Annie Kwan <akwan.mtl@gmail.com>
2021-04-10 16:46:45 -0400

    checking something on heroku

--- blog/views.py
+++ blog/views.py
@@ -19,9 +19,6 @@ class PostListView(ListView):
 
   def get_context_data(self, **kwargs):
     context = super(PostListView, self).get_context_data(**kwargs)
-    # print(context)
-    # context['created'] = timezone.localtime(context['created'])
-
     return context
 
 def add(request):
